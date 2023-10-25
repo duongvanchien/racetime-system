86cbe30f91b57dfa6d96d4896b2899c7986c5d4d
Chien Duong Van <chien.duongvan@powergateaustralia.com>
2023-10-25 20:58:03 +0700

    update

--- env.example.ts
+++ env.example.ts
@@ -1,10 +1,8 @@
-// export const USERNAME = 'FX9600FD37EB';
-// export const PASSWORD = 'Trackify@123';
 export const USERNAME = 'admin';
 export const PASSWORD = 'admin';
 export const MQTT_URL = 'ws://127.0.0.1:8083/mqtt';
-export const MQTT_CLIENT_ID = 'APP-FX9600FD37EB';
-export const MQTT_MANAGEMENT_EVENTS_TOPIC = '/FX9600FD37EB_ssl/tevents';
-export const MQTT_TAG_DATA_EVENTS_TOPIC = '/FX9600FD37EB_ssl/mevents';
-export const MQTT_COMMAND_TOPIC = '/FX9600FD37EB_ssl/cmds';
-export const MQTT_RESPONSE_TOPIC = '/FX9600FD37EB_ssl/rsp';
+export const MQTT_CLIENT_ID = 'RACE TIME APP';
+export const MQTT_MANAGEMENT_EVENTS_TOPIC = '/mevents';
+export const MQTT_TAG_DATA_EVENTS_TOPIC = '/tevents';
+export const MQTT_COMMAND_TOPIC = '/cmds';
+export const MQTT_RESPONSE_TOPIC = '/rsps';
